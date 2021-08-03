6be687c9490246a3bbf1c62fa4d924e6a4f9cc6b
Julian Holfeld <julian@uni-kassel.de>
2021-08-03 19:24:24 +0200

    doc: added documentation for basic events

--- Sources/com.distrust.wakatime.sdPlugin/app.js
+++ Sources/com.distrust.wakatime.sdPlugin/app.js
@@ -9,18 +9,26 @@ function connected() {
 const action = {
     settings: {},
 
+    /**
+     * This event will fire, right before the app will be shown on the Stream Deck.
+     * @param jsn This JSONObject contains the plugin's context.
+     */
     onWillAppear: function (jsn) {
         this.settings = jsn.payload.settings;
 
         if (!this.settings || Object.keys(this.settings).length === 0) {
-            this.settings.mynameinput = 'WAKATIME';
+            this.settings.appname = 'WAKATIME';
         }
         this.setTitle(jsn);
     },
 
+    /**
+     * This method changes the title depending on the value stored in the appname-key.
+     * @param jsn This JSONObject contains the plugin's context.
+     */
     setTitle: function (jsn) {
-        if (this.settings && this.settings.hasOwnProperty('mynameinput')) {
-            $SD.api.setTitle(jsn.context, this.settings.mynameinput);
+        if (this.settings && this.settings.hasOwnProperty('appname')) {
+            $SD.api.setTitle(jsn.context, this.settings.appname);
         }
     },
 };
